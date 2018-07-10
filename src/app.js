573325c84d0260672af1af21a83fb083454a9055
SergeyTolmachev <stolmachov@gmail.com>
2018-07-10 20:05:44 +0300

    with salary filter

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 let mongoose = require('mongoose');
-const vacanciesSchema = require('./src/schemas/vacanciesSchema');
-const db = require('./src/config/mongodbConnect');
+const vacanciesSchema = require('./schemas/vacanciesSchema');
+const db = require('./config/mongodbConnect');
 
 
 let vacanciesModel = mongoose.model('vacanciesModel', vacanciesSchema.vacanciesSchema);
@@ -18,30 +18,31 @@ app.get('/vacancies/', (req, res) => {
     let page;
     let pageSkips;
     let queryToDatabase = {};
+    queryToDatabase.salary = {};
 
 
-    if (req.query.page) {
-        page = req.query.page;
-    } else {
-        page = 1;
-    }
+    page = req.query.page || 1;
 
     pageSkips = (page - 1) * 10;
+    if (req.query.salary === 'true') {
+        queryToDatabase = {'salary.currency': {$ne: null}};
 
-    if (req.query.remote == 'false') {
+    }
+
+    if (req.query.remote === 'false') {
         queryToDatabase.remote = false;
     }
 
-    if (req.query.remote == 'true') {
+    if (req.query.remote === 'true') {
         queryToDatabase.remote = true;
     }
 
 
-    if (req.query.fullDay == 'false') {
+    if (req.query.fullDay === 'false') {
         queryToDatabase.fullDay = false;
     }
 
-    if (req.query.fullDay == 'true') {
+    if (req.query.fullDay === 'true') {
         queryToDatabase.fullDay = true;
     }
 
@@ -50,19 +51,17 @@ app.get('/vacancies/', (req, res) => {
         queryToDatabase.location = 'Россия, ' + req.query.location;
     }
 
+
     console.log(JSON.stringify(queryToDatabase, null, 4));
 
-    vacanciesModel.find(queryToDatabase, {"description": 0, "skills": 0}, function (err, vacancies) {
+    vacanciesModel.find(queryToDatabase, {
+        "description": 0,
+        "skills": 0
+    }, function (err, vacancies) {
         if (err) return console.error(err);
-        //console.log(vacancies);
         res.status(200).json(vacancies);
     }).skip(pageSkips).limit(10).sort({date: -1});
 });
-
-//lastId - номер последней отданной вакансии
-//remote - возможность удаленной работы
-//location - местоположение
-//fullDay - необходимость полного рабочего дня
 
 
 app.get('/vacancies/:vacancy_id', (req, res) => {
@@ -72,13 +71,11 @@ app.get('/vacancies/:vacancy_id', (req, res) => {
         vacanciesModel.findOne({lastId: vacancyId}, function (err, vacancy) {
             if (err) return console.error(err);
             res.status(200).json(vacancy);
-
         });
     }
 });
 
 
 app.listen(3000, () => {
-    console.log('We are live on ' + 3000);
 });
 
