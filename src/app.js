34046fff4269b21f1a1f0f0703c4365a51aa0fa5
SergeyTolmachev <stolmachov@gmail.com>
2018-07-19 18:13:22 +0300

    modified

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 const vacanciesSchema = require('./schemas/vacanciesSchema');
 let vacanciesModel = mongoose.model('vacanciesModel', vacanciesSchema.vacanciesSchema);
-
-
+const getData = require('./utils/getData');
+const getVacancy = require('./utils/getVacancy');
 
 if (cluster.isMaster) {
     cluster.on('disconnect', (worker, code, signal) => {
@@ -29,70 +29,20 @@ if (cluster.isMaster) {
     console.log('hello from worker #' + process.pid);
 
 
-    app.get('/vacancies/', (req, res) => {
-        let page;
-        let pageSkips;
-        let queryToDatabase = {};
-
-
-        page = req.query.page || 1;
-
-        pageSkips = (page - 1) * 10;
-        if (req.query.salary === 'true') {
-            queryToDatabase.salary = {};
-            queryToDatabase = {'salary.currency': {$ne: null}};
-
-        }
-
-        if (req.query.remote === 'true') {
-            queryToDatabase.remote = true;
-        }
-
-
-        if (req.query.fullDay === 'false') {
-            queryToDatabase.fullDay = false;
-        }
-
-        if (req.query.fullDay === 'true') {
-            queryToDatabase.fullDay = true;
-        }
-
-
-        if (req.query.location) {
-            queryToDatabase.location = 'Россия, ' + req.query.location;
-        }
-
-        console.log('Worker ' + process.pid);
-        console.log(JSON.stringify(queryToDatabase, null, 4));
-
-        vacanciesModel.find(queryToDatabase, {
-            "description": 0,
-            "skills": 0
-        }, function (err, vacancies) {
-            if (err) return console.error(err);
-            res.status(200).json(vacancies);
-        }).skip(pageSkips).limit(10).sort({date: -1});
+    app.get('/vacancies/', async (req, res) => {
+        let vacancies = await getData(req);
+        console.log(vacancies);
+        res.status(200).json(vacancies);
     });
 
 
-    app.get('/vacancies/:vacancy_id', (req, res) => {
-        let vacancyId = 0;
-        if (req.params['vacancy_id']) {
-            vacancyId = req.params['vacancy_id'];
-            vacanciesModel.findOne({lastId: vacancyId}, function (err, vacancy) {
-                if (err) {
-                    res.status(404).send('Страница вакансии не найдена');
-                    return console.error(err);
-                }
-                if (vacancy === null){
-                    res.status(404).send('Страница вакансии не найдена');
-
-                } else {
-                    res.status(200).json(vacancy);
-                }
-            });
+    app.get('/vacancies/:vacancy_id', async (req, res) => {
+        let vacancy = await getVacancy(req);
+        console.log(vacancy);
+        if (vacancy){
+            res.status(200).json(vacancy);
         } else {
-            res.status(404).send('Страница вакансии не найдена');
+            res.status(404).send('страница вакансии не найдена');
         }
     });
 
